89c165f3756f86fe6aa10567612226b9b0199bfd
Vladimir Drayling <>
2024-12-14 11:53:25 +0100

    chore(packages): update angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  template: `<router-outlet name="nav" /><router-outlet />`,
+  templateUrl: './app.component.html',
   imports: [RouterOutlet],
 })
 export class AppComponent {}
